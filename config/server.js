99774fea5539eec378ef8e7f58a71d093b4e2275
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2022-06-14 11:21:56 +0200

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3ae2d1585c6934f54beb43905c243256'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e36f2a60-8c61-4714-8333-49350f873d7a'
+};
